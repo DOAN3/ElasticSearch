56dd124e85d2d15bd6775878c6d0c17d02e614e4
Tri Nguyen Duong Thanh <36912330+ttrios@users.noreply.github.com>
2018-12-27 10:10:17 +0700

    ccc

---
+++ web/hoalan/src/index/index.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { IndexComponent } from './index.component';
+
+describe('IndexComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        IndexComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(IndexComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'hoalan'`, () => {
+    const fixture = TestBed.createComponent(IndexComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('hoalan');
+  });
+
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(IndexComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hoalan!');
+  });
+});
---
+++ web/hoalan/src/index/index.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent {
+  title = 'hoalan';
+}
